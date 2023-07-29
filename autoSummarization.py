cb47a5b70d9ebb04f8d277435eb2596ff5367ded
keu-5 <135619670+keu-5@users.noreply.github.com>
2023-07-29 12:21:47 +0900

    Add files via upload
    openAI公式サイトにて事前にAPIキーを取得してください

---
+++ autoSummarization.py
@@ -0,0 +1,69 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+import openai
+openai.organization = "org-YupUjIebSYVcUh0Gu3tqBieD"
+openai.api_key = "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"
+
+#本番用: TWqJ5P8oaUM
+#テスト用: q62z-Iemp5U
+
+#動画のIDを渡す
+transcript_list = YouTubeTranscriptApi.list_transcripts("RGYTGmUwseE")
+
+sentenceList = []
+
+#文字起こしデータを出力(リスト形式に変換)
+for transcript in transcript_list:
+    for tr in transcript.fetch():
+        print(tr)
+        sentenceList.append(tr['text'])
+
+#n分割関数
+def split_list(l, n):
+    """
+    リストをサブリストに分割する
+    :param l: リスト
+    :param n: サブリストの要素数
+    :return: 
+    """
+    for idx in range(0, len(l), n):
+        yield l[idx:idx + n]
+
+#リストを三分割
+result = list(split_list(sentenceList, len(sentenceList) // 3))
+
+for i in range(3):
+    result[i] = '、'.join(result[i])
+    result[i].replace('[音楽]、', '')
+
+    result[i] = f"「{result[i]}」を要約してください"
+
+def Ask_ChatGPT(message):
+    # 応答設定
+    completion = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo", # モデルを選択
+        messages = [{
+            "role":"user", # 役割
+            "content":message, # メッセージ 
+        }],
+    
+        max_tokens = 1024, # 生成する文章の最大単語数
+        n = 1, # いくつの返答を生成するか
+        stop = None, # 指定した単語が出現した場合、文章生成を打ち切る
+        temperature = 0.5, # 出力する単語のランダム性（0から2の範囲） 0であれば毎回返答内容固定
+    )
+    # 応答
+    response = completion.choices[0].message.content
+    # 応答内容出力
+    return response
+
+
+#要約第一段階
+for i in range(len(result) - 1):
+    
+    # 質問内容
+    message = str(result[i])
+    # ChatGPT起動
+    res = Ask_ChatGPT(message)
+    # 出力
+    print(res)
+
